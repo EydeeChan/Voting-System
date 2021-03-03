2fd8122a26d2ee2121e5f969c5c219373cc3ac50
Kenneth Glenn Somera Bamba <dev.eydeechan@gmail.com>
2021-03-03 12:04:40 +0800

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>Taytay Senior High School</b>
     </div>
-    <strong> &copy; 2021 Brought To You By <a href="https://githb.com/EydeeChan">Kenneth Glenn Bamba</a></strong>
+    <strong> &copy; 2021 Brought To You By <a href="https://github.com/EydeeChan">Kenneth Glenn Bamba</a></strong>
 </footer>
